e69764849c316e72c9d5bd2e36f2257602e84933
camber01 <bernabe.cambarihan@gcash.com>
2022-04-28 12:04:57 +0800

    Register

--- src/test/TestSMSChecker.java
+++ src/test/TestSMSChecker.java
@@ -5,12 +5,20 @@ import org.junit.Test;
 import utility.Helper;
 
 import java.io.IOException;
+import static org.junit.Assert.*;
 
 public class TestSMSChecker {
 
     @Test
     public void testCheckRegister() {
-
+        assertTrue(SMSChecker.checkRegister("Register"));
+        assertTrue(SMSChecker.checkRegister("ReGisTer"));
+        assertTrue(SMSChecker.checkRegister("register"));
+        assertTrue(SMSChecker.checkRegister("REGISTER"));
+        assertFalse(SMSChecker.checkRegister(" register"));
+        assertFalse(SMSChecker.checkRegister("register "));
+        assertFalse(SMSChecker.checkRegister("random"));
+        assertFalse(SMSChecker.checkRegister(""));
     }
 
     @Test
