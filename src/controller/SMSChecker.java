709bd9f86e0bdcc04a3300c24c2ea31d2ed8c7cb
camber01 <bernabe.cambarihan@gcash.com>
2022-04-28 12:04:57 +0800

    Added checkRegister and testCheckRegister

--- src/controller/SMSChecker.java
+++ src/controller/SMSChecker.java
@@ -1,19 +1,26 @@
 package controller;
 
+import refactor.Registration;
+
+import java.util.Locale;
+
 public class SMSChecker {
-    private boolean validRegistration = false;
+    private static boolean validRegistration = false;
     private boolean validUserInformation = false;
     private boolean validName = false;
     private boolean validBirthdate = false;
     private boolean validAddress = false;
 
     public static boolean checkRegister(String register){
-        return true;
+
+        if(register.toLowerCase(Locale.ROOT).equals("register")){
+            validRegistration = true;
+            return true;
+        }
+        return false;
     }
 
     public static boolean checkUserInfo(String register){
         return true;
     }
-
-
 }
